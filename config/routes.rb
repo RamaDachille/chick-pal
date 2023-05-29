537d925c3b04bb45547b8a04b872bd1a0badbd04
bruno belchior <brunobelchior@hotmail.com>
2023-05-29 17:38:15 +0200

    routes done

--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,8 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  resources :chickens, except: %i[edit update] do
+    resources :bookings, only: %i[new create]
+  end
+  resources :bookings, only: %i[show update]
 end
