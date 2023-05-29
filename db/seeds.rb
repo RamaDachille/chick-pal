42c434753ab49d501c1dfd95a75078406e32515b
Joanna Wojtowicz <joanna.grazyna.wojtowicz@gmail.com>
2023-05-29 17:45:11 +0200

    Add Users and Chickens/seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,6 +5,8 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+require 'faker'
+
 puts "cleaning DB"
 
 User.destroy_all
@@ -12,16 +14,34 @@ User.destroy_all
 puts "DB preparation"
 puts "creating Users"
 
-10.times User.new(first_name:
-                  last_name:
-                  email:
-                  password:
-                  biography:
-                  )
+User.new(first_name: "Joanna",
+        last_name: "Pink",
+        email: "joanna@example.com",
+        password: 123456,
+        biography: "Owning a farm with chickens has been a fulfilling adventure. The joy of tending to the land and witnessing
+        the daily egg-laying miracles is truly rewarding."
+        )
+
+10.times do
+  User.new(first_name: Faker::Beer.brand,
+          last_name: Faker::Color.color_name,
+          email: Faker::Internet.email,
+          password: 123456,
+          biography: Faker::Quotes::Shakespeare.as_you_like_it_quote
+          )
+end
 
 
 puts "creating Chickens"
 
-20.times
+20.times do
+  Chicken.new(age: rand(1..10),
+              breed: ["Andalusian", "Brahma", "Brakel", "Dutch Bantam", "Hamburgh"].sample,
+              egg_capacity: rand(1..5),
+              gender: Faker::Gender.binary_type,
+              noise_level: ["low", "medium", "high"].sample,
+              description: Faker::Coffee.notes
+              )
+end
 
 puts "👩🏻‍🦳👨🏿‍🦱 and 🐔 have been sucessfuly created "
