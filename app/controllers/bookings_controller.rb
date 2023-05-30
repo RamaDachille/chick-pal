7ec556af9e8d9a254b2ed02055c3a5cc3ae21b36
RamaDachille dachille108@gmail.com <dachille108@gmail.com>
2023-05-30 17:05:15 +0200

    fixed controller methods, add views, add edit route

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -22,14 +22,23 @@ class BookingsController < ApplicationController
     end
   end
 
+  def edit
+    @booking = Booking.find(params[:id])
+  end
+
   def update
     @booking = Booking.find(params[:id])
-    @booking.update(booking_params)
+    @booking.update(update_params)
+    redirect_to booking_path(@booking)
   end
 
   private
 
   def booking_params
     params.require(:booking).permit(:start_date, :end_date, :chicken_id)
+  end
+
+  def update_params
+    params.require(:booking).permit(:booking_status)
   end
 end
